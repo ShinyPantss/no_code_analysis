e1c36e4ebbeb8b6937199e260a8ff6f89614212d
ShinyPantss <mucahitcan39@hotmail.com>
2024-04-10 22:30:58 +0300

    Fix package.json// Added Card To do

--- app/(home)/datasets/page.tsx
+++ app/(home)/datasets/page.tsx
@@ -11,23 +11,28 @@ type Props = {
   };
 };
 
-const constans = {
-  
-}
+
 
 const DataSets = (props: Props) => {
   return (
 
     <div className="flex items-center justify-center px-4">
       <div className="grid lg:grid-cols-4  md:grid-cols-3 grid-cols-2   justify-center items-center   gap-10 mt-12">
-        {props.data.name &&
+        {/** TODO: Will be applied generic card  */}
+
+        {/* {props.data.name &&
           Object.values(props).map((key) => {
             return (
               <div key={props.data.name} className="">
-                <Card imgUrl={key.imgUrl} name={key.name} />
+                <Card  />
               </div>
             );
-          })}
+          })} */}
+
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
       </div>
     </div>
 
