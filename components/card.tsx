d0eb04aef88c97d975267aa35cde95bc0bfd18cf
Mahmut Ali Öncel <moncel1091@gmail.com>
2024-04-12 14:19:02 +0300

    gg
    problem

--- components/card.tsx
+++ components/card.tsx
@@ -5,19 +5,24 @@ import React from "react";
 
 const Card = () => {
   return (
-    <Link
-      className="bg-stone-900  text-white text-4xl border border-white rounded-xl overflow-hidden shadow-lg relative max-sm:h-28 max-sm:w-36 md:h-52 md:w-60 flex justify-center items-center  "
-      href={"/"}
-    >
-      <Image
-        src="/deneme.jpeg"
-        alt=""
-        width={300}
-        height={200}
-        className="h-full w-full absolute opacity-70  hover:opacity-90 transition-all  "
-      />
-      <p className="relative  mb-4 max-sm:text-xl">Corona</p>
-    </Link>
+    <div className="w-[300px] h-[420px] bg-transparent cursor-pointer group perspective">
+      <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+        <div className="absolute backface-hidden border-2 w-full h-full">
+          <img src="https://www.sserc.org.uk/wp-content/uploads/2023/12/Data-sets-tile.png" alt="" className="w-full h-full" />
+        </div>
+        <div className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-gray-100 overflow-hidden">
+          <div className="text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24">
+            <h1 className="text-3xl font-semibold">The King's Man</h1>
+            <p className="my-2">9.0 Rating</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat error ea iusto et cupiditate animi dicta, fuga suscipit odio perferendis provident officiis asperiores odit eligendi velit corrupti iure! Inventore velit eos excepturi earum aliquam. Eos nobis, adipisci voluptatem nulla eaque quia, in harum facilis deleniti quam vero! Nostrum, minima sunt.</p>
+            <button className="bg-teal-500 px-6 py-2 font-semibold text-white rounded-full absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-20 scale-0 group-hover:scale-125">
+              Watch Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,34 @@
 import type { Config } from "tailwindcss";
+const plugin = require("tailwindcss/plugin");
+
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+module.exports = {
+  mode: "jit",
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [Myclass],
+};
+
+
+
 
 const config = {
   darkMode: ["class"],
